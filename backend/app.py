38d47ddac77ba94e837dcc8ac8fdfebc0f86c982
Ramos-17 <gramos0617@gmail.com>
2026-01-21 13:22:45 -0800

    Added Backend + Frontend

---
+++ backend/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+# Create a Flask application instance
+app = Flask(__name__)
+
+# Define a route for the homepage ('/')
+@app.route('/')
+def hello_world():
+    return 'Hello, World! This is my first Flask app.'
+
+# Run the application if the script is executed directly
+if __name__ == '__main__':
+    app.run(debug=True)
