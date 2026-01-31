f489bff98aeebd97f0034a88a97e8edc1fad57fb
Milton Castellanos <mil2boss4u@Mac.lan>
2026-01-30 16:52:42 -0800

    logIn

--- frontend/react-frontend/src/App.jsx
+++ frontend/react-frontend/src/App.jsx
@@ -1,15 +1,17 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/home.jsx'
+import Login from './pages/logIn.jsx'
 import './App.css'
 
 export default function App() {
-  return (
-      <BrowserRouter>
-        <div className="min-h-screen bg-gradient-to-b from-white to-black">
-          <Routes>
-            <Route path="/" element={<Home />} />
-          </Routes>
-        </div>
-      </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<div className="min-h-screen">
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/login" element={<Login />} />
+				</Routes>
+			</div>
+		</BrowserRouter>
+	);
 }
--- frontend/react-frontend/src/pages/home.jsx
+++ frontend/react-frontend/src/pages/home.jsx
@@ -1,31 +1,40 @@
+import { useNavigate } from "react-router-dom";
+
 export default function Home() {
-    return (
-        <main className="min-h-[100svh] flex items-center justify-center bg-gradient-to-b from-white to-white">
-      <section className="text-center">
-        <h1 className="mb-10 text-5xl font-extrabold text-black">
-          Main Menu
-        </h1>
+	const navigate = useNavigate();
 
-        <div className="flex flex-col gap-4">
-          <button
-            className="rounded-xl px-8 py-4 text-lg font-semibold border border-blue-300 text-black hover:bg-white hover:text-white transition"
-          >
-            Start Game
-          </button>
+	const goToLogin = () => {
+		navigate("/login");
+	};
 
-          <button
-            className="rounded-xl px-8 py-4 text-lg font-semibold border border-blue-300 text-black hover:bg-white hover:text-white transition"
-          >
-            Settings
-          </button>
+	return (
+		<main className="min-h-[100svh] flex items-center justify-center bg-gradient-to-b from-white to-white">
+			<section className="text-center">
+				<h1 className="mb-10 text-5xl font-extrabold text-black">
+					Main Menu
+				</h1>
 
-          <button
-              className="rounded-xl px-8 py-4 text-lg font-semibold border border-blue-300 text-black hover:bg-white hover:text-white transition"
-          >
-            Login - Sign Up
-          </button>
-        </div>
-      </section>
-    </main>
-    );
-   } 
+				<div className="flex flex-col gap-4">
+					<button
+						className="rounded-xl px-8 py-4 text-lg font-semibold border border-blue-300 text-black hover:bg-white hover:text-white transition"
+					>
+						Start Game
+					</button>
+
+					<button
+						className="rounded-xl px-8 py-4 text-lg font-semibold border border-blue-300 text-black hover:bg-white hover:text-white transition"
+					>
+						Settings
+					</button>
+
+					<button
+						className="rounded-xl px-8 py-4 text-lg font-semibold border border-blue-300 text-black hover:bg-white hover:text-white transition"
+						onClick={goToLogin}
+					>
+						Login - Sign Up
+					</button>
+				</div>
+			</section>
+		</main>
+	);
+} 
